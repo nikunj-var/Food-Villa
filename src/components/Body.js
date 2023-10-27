06f026b863e3642bf92e48ced7fffd1b5364f393
Nikunj <nikunjvar2002@gmail.com>
2023-10-27 23:27:25 +0530

    api data fetched

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { restaurantList } from "../Config";
 import RestaurantCard from "./RestaurantCard";
 
@@ -25,16 +25,35 @@ const Body = () => {
   // e.target.value - whatever we write in input
   // one Way data binding
   // useState hook returns a array of variable and function to update this variable
+
   const [inputValue, setInputValue] = useState("");
   const [searchClicked, setSearchClicked] = useState("false");
   const [restaurants, setRestaurants] = useState(restaurantList);
+
   const filterData = (inputValue, restaurants) => {
     const filterdata = restaurants.filter((restaurant) => {
-      restaurant.data.name.includes(inputValue);
+      restaurant.info.name.toLowerCase().includes(inputValue.toLowerCase());
     });
     return filterdata;
   };
-  return (
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.91378&lng=78.080016&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const res = await data.json();
+    setRestaurants(
+      res.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  }
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  return restaurants.length === 0 ? (
+    <h1>Not present</h1>
+  ) : (
     <>
       <div className="search-container">
         <input
@@ -57,7 +76,7 @@ const Body = () => {
       <div className="restaurant-list">
         {restaurants.map((restaurant) => {
           return (
-            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+            <RestaurantCard {...restaurant.info} key={restaurant?.info?.id} />
           );
         })}
       </div>
